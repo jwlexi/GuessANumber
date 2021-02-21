0a7ab82dc3cba0f695e8df374ba512b382ca431e
Alexis Sanchez <alexisanchez@csumb.edu>
2021-02-21 23:29:25 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,83 @@
+/*global $*/
+var randomNumber = Math.floor(Math.random() * 99) + 1;
+
+var numOfTries = 1;
+var guesses = "Previous guesses: ";
+var wins = 0;
+var losses = 0;
+var winCount = $("#win-count");
+var loseCount = $("#lose-count");
+var box = $("#userNum");
+var resetGame = false;
+document.querySelector("#submitGuess").addEventListener("click", submitGuess);
+
+document.querySelector(".verdict").innerHTML = "Verdict will display here";
+document.querySelector(".verdict").style.backgroundColor = "lightgray";
+document.querySelector(".prev-guesses").innerHTML = guesses;
+
+function reset() {
+  randomNumber = Math.floor(Math.random() * 99) + 1;
+  numOfTries = 1;
+  guesses = "Previous guesses: ";
+  box.val(" ");
+  document.querySelector(".prev-guesses").innerHTML = guesses;
+  document.querySelector(".verdict").innerHTML = "Verdict will display here";
+  document.querySelector(".verdict").style.backgroundColor = "lightgray";
+  document.querySelector("#submitGuess").innerHTML = "Submit Guess";
+  resetGame = false;
+}
+
+function submitGuess() {
+
+  if (resetGame) {
+    reset();
+    return;
+  }
+
+  let userInput = document.querySelector("#userNum").value;
+  let verdict = "";
+  let verdictBackgroundColor = "";
+
+  if (numOfTries > 7 || (numOfTries == 7 && userInput != randomNumber)) {
+    alert("No more tries left, submit to start over");
+    document.querySelector(".verdict").style.backgroundColor = "red";
+    losses++;
+    loseCount.html(losses);
+    box.val(" ");
+    reset();
+    return;
+  }
+
+  if (isNaN(userInput) || userInput < 1 || userInput > 99) {
+    alert("Invalid input, enter a number between 1 and 99.");
+  }
+  else {
+    guesses += userInput + ' ';
+    numOfTries++;
+
+    document.querySelector(".prev-guesses").innerHTML = guesses;
+
+    if (userInput < randomNumber) {
+      verdict = "Wrong Guess - Too Low";
+      box.val(" ");
+      verdictBackgroundColor = "red";
+    } else if (userInput > randomNumber) {
+      verdict = "Wrong Guess - Too High";
+      box.val(" ");
+      verdictBackgroundColor = "red";
+    } else if (userInput == randomNumber) {
+      verdict = "Correct Guess - Congrats!";
+      box.val(" ");
+      verdictBackgroundColor = "green";
+      wins++;
+      winCount.html(wins);
+      document.querySelector(".verdict").innerHTML = verdict;
+      document.querySelector(".verdict").style.backgroundColor = verdictBackgroundColor;
+      resetGame = true;
+      document.querySelector("#submitGuess").innerHTML = "Restart";
+      return;
+    }
+    document.querySelector(".verdict").innerHTML = verdict;
+    document.querySelector(".verdict").style.backgroundColor = verdictBackgroundColor;
+  }
+}
